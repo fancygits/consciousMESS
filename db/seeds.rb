509f319579be3a562925538ee7a1e3bd79aa8f1a
James Luke Johnson <jjohn144@my.westga.edu>
2019-03-05 19:43:17 -0500

    Formatted structure of Ripple form and index.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ripple.delete_all
+
+Ripple.create!(name: 'QwertyMan', 
+                url: 'www.jinglebells.com', 
+                message: %{Have a holly jolly Christmas... ...it's the best time of the year.})
+
+Ripple.create!(name: 'Dr. Claw',
+                url: 'www.clawindustries.business',
+                message: 'NEXT TIME GADGET!')
+
+Ripple.create!(name: 'Anon E. Mouse',
+                url: 'www.4chan.com',
+                message: %{Can't touch this (duh duh duh duh, duh duh, duh duh)})
+Ripple.create!(name: 'Donec',
+                message: 'ullamcorper viverra. Maecenas iaculis aliquet diam. Sed 
+                          diam dorem, auctor quinon.')
